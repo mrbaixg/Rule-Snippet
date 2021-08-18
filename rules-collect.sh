be82c7797ba9c4ec54e6b77896105d0af745086a
baixg <15259109+MrBaixg@users.noreply.github.com>
2021-08-19 00:55:19 +0800

    Added some rules for clash

--- rules-collect.sh
+++ rules-collect.sh
@@ -2,32 +2,53 @@
 
 
 # Rules
-if [ ! -d "./Rules/Basic" ]; then
-  mkdir -p ./Rules/Basic
+if [ ! -d "./Rules/QuantumultX/Basic" ]; then
+  mkdir -p ./Rules/QuantumultX/Basic
 fi
 
-find ./Rules/Basic -type f -name '*.list' -delete
+find ./Rules/QuantumultX/Basic -type f -name '*.list' -delete
 
-curl -o ./Rules/Basic/Apple-News.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/Apple-News.list
-curl -o ./Rules/Basic/Apple-proxy.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/Apple-proxy.list
-curl -o ./Rules/Basic/Apple-direct.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/Apple-direct.list
-curl -o ./Rules/Basic/CN.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/CN.list
-curl -o ./Rules/Basic/LAN.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/LAN.list
-curl -o ./Rules/Basic/common-ad-keyword.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/common-ad-keyword.list
-curl -o ./Rules/Basic/foreign.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/foreign.list
+curl -o ./Rules/QuantumultX/Basic/Apple-News.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/Apple-News.list
+curl -o ./Rules/QuantumultX/Basic/Apple-proxy.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/Apple-proxy.list
+curl -o ./Rules/QuantumultX/Basic/Apple-direct.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/Apple-direct.list
+curl -o ./Rules/QuantumultX/Basic/CN.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/CN.list
+curl -o ./Rules/QuantumultX/Basic/LAN.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/LAN.list
+curl -o ./Rules/QuantumultX/Basic/common-ad-keyword.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/common-ad-keyword.list
+curl -o ./Rules/QuantumultX/Basic/foreign.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/foreign.list
 
 
-if [ ! -d "./Rules/App/social" ]; then
-  mkdir -p ./Rules/App/social
+if [ ! -d "./Rules/QuantumultX/App/social" ]; then
+  mkdir -p ./Rules/QuantumultX/App/social
 fi
 
-find ./Rules/App/social -type f -name '*.list' -delete
+find ./Rules/QuantumultX/App/social -type f -name '*.list' -delete
 
-curl -o ./Rules/App/social/Telegram.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/App/social/Telegram.list
-curl -o ./Rules/App/social/WhatsApp.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/App/social/WhatsApp.list
+curl -o ./Rules/QuantumultX/App/social/Telegram.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/App/social/Telegram.list
+curl -o ./Rules/QuantumultX/App/social/WhatsApp.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/App/social/WhatsApp.list
 
-find ./Rules/ -maxdepth 1 -type f -name '*.list' -delete
-curl -o ./Rules/rewrite.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/rewrite.list
+find ./Rules/QuantumultX -maxdepth 1 -type f -name '*.list' -delete
+curl -o ./Rules/QuantumultX/rewrite.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/rewrite.list
+
+if [ ! -d "./Rules/Clash/Basic" ]; then
+  mkdir -p ./Rules/Clash/Basic
+fi
+
+find ./Rules/Clash/Basic -type f -name '*.yaml' -delete
+
+curl -o ./Rules/Clash/Basic/Apple-proxy.yaml https://github.com/Hackl0us/SS-Rule-Snippet/raw/master/Rulesets/Clash/Basic/Apple-proxy.yaml
+curl -o ./Rules/Clash/Basic/Apple-direct.yaml https://github.com/Hackl0us/SS-Rule-Snippet/raw/master/Rulesets/Clash/Basic/Apple-direct.yaml
+curl -o ./Rules/Clash/Basic/CN.yaml https://github.com/Hackl0us/SS-Rule-Snippet/raw/master/Rulesets/Clash/Basic/CN.yaml
+curl -o ./Rules/Clash/Basic/common-ad-keyword.yaml https://github.com/Hackl0us/SS-Rule-Snippet/raw/master/Rulesets/Clash/Basic/common-ad-keyword.yaml
+curl -o ./Rules/Clash/Basic/foreign.yaml https://github.com/Hackl0us/SS-Rule-Snippet/raw/master/Rulesets/Clash/Basic/foreign.yaml
+curl -o ./Rules/Clash/Basic/LAN.yaml https://github.com/Hackl0us/SS-Rule-Snippet/raw/master/Rulesets/Clash/Basic/LAN.yaml
+
+if [ ! -d "./Rules/Clash/App/social" ]; then
+  mkdir -p ./Rules/Clash/App/social
+fi
+
+find ./Rules/Clash/App/social -type f -name '*.yaml' -delete
+
+curl -o ./Rules/Clash/App/social/Telegram.yaml https://github.com/Hackl0us/SS-Rule-Snippet/raw/master/Rulesets/Clash/App/social/Telegram.yaml
 
 
 # Scripts
