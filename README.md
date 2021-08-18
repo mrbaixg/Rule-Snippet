11a5c58da0176c4277d313be6072a4b16f3d364a
baixg <15259109+MrBaixg@users.noreply.github.com>
2021-08-18 22:33:30 +0800

    Initial Commit
    Finished First draft.

---
+++ README.md
@@ -0,0 +1,8 @@
+# 规则/脚本
+
+本仓库不定期从网络中更新Quantumult X适用的规则和脚本，仅供本人使用。
+
+
+规则部分主要使用了[@Hackl0us](https://github.com/Hackl0us)的[SS-Rule-Snippet](https://github.com/Hackl0us/SS-Rule-Snippet)。
+
+脚本部分主要使用了[@NobyDa](https://github.com/NobyDa)的[Script](https://github.com/NobyDa/Script)。
---
+++ rules-collect.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+# Rules
+if [ ! -d "./Rules/Basic" ]; then
+  mkdir -p ./Rules/Basic
+fi
+
+find ./Rules/Basic -type f -name '*.list' -delete
+
+curl -o ./Rules/Basic/Apple-News.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/Apple-News.list
+curl -o ./Rules/Basic/Apple-proxy.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/Apple-proxy.list
+curl -o ./Rules/Basic/Apple-direct.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/Apple-direct.list
+curl -o ./Rules/Basic/CN.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/CN.list
+curl -o ./Rules/Basic/LAN.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/LAN.list
+curl -o ./Rules/Basic/common-ad-keyword.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/common-ad-keyword.list
+curl -o ./Rules/Basic/foreign.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/Basic/foreign.list
+
+
+if [ ! -d "./Rules/App/social" ]; then
+  mkdir -p ./Rules/App/social
+fi
+
+find ./Rules/App/social -type f -name '*.list' -delete
+
+curl -o ./Rules/App/social/Telegram.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/App/social/Telegram.list
+curl -o ./Rules/App/social/WhatsApp.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/App/social/WhatsApp.list
+
+find ./Rules/ -maxdepth 1 -type f -name '*.list' -delete
+curl -o ./Rules/rewrite.list https://raw.githubusercontent.com/Hackl0us/SS-Rule-Snippet/master/Rulesets/Quantumult/rewrite.list
+
+
+# Scripts
+if [ ! -d "./Scripts" ]; then
+  mkdir -p ./Scripts
+fi
+
+find ./Scripts -type f -name '*.js' -delete
+curl -o ./Scripts/PolicySwitch.js https://raw.githubusercontent.com/NobyDa/Script/master/Shortcuts/PolicySwitch.js
+curl -o ./Scripts/JD_DailyBonus.js https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js
+curl -o ./Scripts/Wechat.js https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+curl -o ./Scripts/chavy.box.js https://raw.githubusercontent.com/chavyleung/scripts/master/chavy.box.js
